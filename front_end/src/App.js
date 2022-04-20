dc96057a5e24ca6b2c2b32385fad745bdc2f946d
monkeyapple <monkeyappleh@gmail.com>
2022-04-20 22:52:29 +0800

    try

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { parseWithOptions } from 'date-fns/fp';
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect,useCallback} from "react";
+import React, { useState, useEffect} from "react";
 import { DataFactory } from './components/DataFactory';
 import GroupAssets from './GroupAssets';
 import ViewCharts from './components/ViewCharts';
@@ -18,18 +18,19 @@ function App() {
       try{
         // const response=await axios.get("http://localhost:5000/fetch") 
         const response=await axios.get("https://queendom2.herokuapp.com/fetch") 
-        console.log(response.data)
-        const data1=DataFactory(response.data.per1)
-        const data2=DataFactory(response.data.per2)
+        const data1=DataFactory(await response.data.per1)
+        const data2=DataFactory(await response.data.per2)
+        console.log('data1 is'+data1)
         SetPer1Data(data1) 
         SetPer2Data(data2)
+        
       }catch(error){
         console.error(error);
       }
     }; 
     fetchDataHandler();
   },[])
-
+  console.log('per1data is:'+per1data)
   return(
     <div>
       <div className='container-fluid'>
