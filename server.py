35b4a37459c6964067196bab0c3b348575c4f244
monkeyapple <monkeyappleh@gmail.com>
2022-04-20 21:42:45 +0800

    try

--- server.py
+++ server.py
@@ -1,12 +1,11 @@
 from flask import jsonify,Flask,send_from_directory
-from flask_cors import CORS,cross_origin
+from flask_cors import CORS
 from dboperations import DatabaseOperate
 app=Flask(__name__,static_folder='front_end/build',static_url_path='')
 CORS(app)
 
 
 @app.route('/fetch')
-@cross_origin()
 def fetch_all_groups():
     dbOp=DatabaseOperate()
     table1Rows=dbOp.query_table1()
@@ -14,10 +13,9 @@ def fetch_all_groups():
     return jsonify({'per1':table1Rows,'per2':table2Rows})
     
 @app.route('/')
-@cross_origin()
 def serve():
     return send_from_directory(app.static_folder,'index.html')
 
 if __name__=='__main__':
     # app.run()
-    app.run(port=5000,debug=True)
+    app.run(host='0.0.0.0',debug=True)
