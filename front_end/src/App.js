35dcba2e1fbb6465ee1a2a5edb5fae936720460f
monkeyapple <monkeyappleh@gmail.com>
2022-04-20 22:13:22 +0800

    try

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -3,59 +3,33 @@ import { parseWithOptions } from 'date-fns/fp';
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect,useCallback} from "react";
+import { DataFactory } from './components/DataFactory';
 import GroupAssets from './GroupAssets';
 import ViewCharts from './components/ViewCharts';
-import { DataFactory } from './components/DataFactory';
 import Footer from './components/Footer';
 
-
 function App() {
-  const [isLoading,setIsLoading]=useState(false);
-  const [error,setError]=useState(null);
   const[per1data,SetPer1Data]=useState({});
   const[per2data,SetPer2Data]=useState({});
 
   // Solution1: use axios 
-  //   useEffect(()=>{
-  //   const fetchDataHandler=async()=>{
-  //     try{
-  //       // const response=await axios.get("http://localhost:5000/fetch") 
-  //       const response=await axios.get("https://queendom2.herokuapp.com/fetch") 
-  //       const data1=DataFactory(response.data.per1)
-  //       const data2=DataFactory(response.data.per2)
-  //       SetPer1Data(data1) 
-  //       SetPer2Data(data2)
-  //     }catch(error){
-  //       console.error(error);
-  //     }
-  //   }; 
-  //   fetchDataHandler();
-  // },[])
-
-  // Solution2:use built-in fetch
-  const fetchDataHandler=useCallback(async()=>{
-    setIsLoading(true);
-    setError(null);
-    try{
-      const response=await fetch('/fetch');
-      // const response=await fetch("http://localhost:5000/fetch") 
-      if (!response.ok){
-        throw new Error('Something went wrong while fetching data');
+    useEffect(()=>{
+    const fetchDataHandler=async()=>{
+      try{
+        // const response=await axios.get("http://localhost:5000/fetch") 
+        const response=await axios.get("https://queendom2.herokuapp.com/fetch") 
+        console.log(response.data)
+        const data1=DataFactory(response.data.per1)
+        const data2=DataFactory(response.data.per2)
+        SetPer1Data(data1) 
+        SetPer2Data(data2)
+      }catch(error){
+        console.error(error);
       }
-      const rawData=await response.json()
-      SetPer1Data(DataFactory(rawData.per1)) 
-      SetPer2Data(DataFactory(rawData.per2))
-    }catch(error){
-      setError(error.message);
-    }
-    setIsLoading(false);
-  },[]);
-  useEffect(()=>{
+    }; 
     fetchDataHandler();
-  },[fetchDataHandler]);
+  },[])
 
-
-  console.log(per1data)
   return(
     <div>
       <div className='container-fluid'>
