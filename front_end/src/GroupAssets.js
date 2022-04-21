e5e5de4332eb184880e208b2103d28f815b2ee9f
monkeyapple <monkeyappleh@gmail.com>
2022-04-21 23:23:30 +0800

    update with per2 videos

--- front_end/src/GroupAssets.js
+++ front_end/src/GroupAssets.js
@@ -9,16 +9,16 @@ import wjsn_per1 from "./images/wjsn_per1.jpeg"
 import bravegirls_per2 from "./images/bravegirls_per2.jpeg"
 import wjsn_per2 from "./images/wjsn_per2.jpeg"
 import loona_per2 from "./images/loona_per2.jpeg"
-
-import soonImage from "./images/soonImage.jpeg"
-
+import hyolyn_per2 from "./images/hyolyn_per2.jpeg"
+import ke1per_per2 from "./images/ke1per_per2.jpeg"
+import viviz_per2 from "./images/viviz_per2.jpeg"
 
 const GroupAssets=[
 
     {
         name:"Hyolyn",
-        imgURL:[hyolyn_per1,soonImage],
-        videoLink:["https://www.youtube.com/watch?v=2D2FoanXVL0","#"]
+        imgURL:[hyolyn_per1,hyolyn_per2],
+        videoLink:["https://www.youtube.com/watch?v=2D2FoanXVL0","https://www.youtube.com/watch?v=BEZk9QVjA-s"]
     },
     {
         name:"Brave Girls",
@@ -37,13 +37,13 @@ const GroupAssets=[
     },
     {
         name:"Viviz",
-        imgURL:[viviz_per1,soonImage],
-        videoLink:["https://www.youtube.com/watch?v=rY8Gz8bBoR8","#"]
+        imgURL:[viviz_per1,viviz_per2],
+        videoLink:["https://www.youtube.com/watch?v=rY8Gz8bBoR8","https://www.youtube.com/watch?v=NyjWRuazgsw"]
     },
     {
         name:"Ke1per",
-        imgURL:[ke1per_per1,soonImage],
-        videoLink:["https://www.youtube.com/watch?v=THiACt2pURE","#"]
+        imgURL:[ke1per_per1,ke1per_per2],
+        videoLink:["https://www.youtube.com/watch?v=THiACt2pURE","https://www.youtube.com/watch?v=EzJfJhznH3s"]
     }
 
 ]
