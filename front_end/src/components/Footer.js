c55bc90f5f5983c13e219b07d5bced610396c82c
monkeyapple <monkeyappleh@gmail.com>
2022-04-21 23:29:17 +0800

    update twitter link to specific post

--- front_end/src/components/Footer.js
+++ front_end/src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer=()=>{
         <footer class="footer-section">
             <div class="container-fluid d-flex justify-content-end">
                 <a class="btn btn-link btn-floating btn-lg text-white m-1"
-                    href="https://twitter.com/EffyLH20"
+                    href="https://twitter.com/EffyLH20/status/1517019909921787904?s=20&t=u0W0ktzwsvnbc3pteKYdIg"
                     role="button"><AiFillTwitterCircle color="cornflowerblue" size="2.5em"/>
                 </a>
       
